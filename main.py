be203c0b39de750a135fe5e692f3be237500d268
DenMaslov <76794599+DenMaslov@users.noreply.github.com>
2021-06-17 19:40:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from game import Game
 from army import Army
 from reader import Reader
 
-def random_armies():
+def random_armies() -> None:
     army1 = Army("east")
     army2 = Army("west")
     army1.fill_random_squads(r.randint(Game.MIN_SQUADS, Game.MAX_SQUADS))
@@ -14,18 +14,17 @@ def random_armies():
     game.add_army(army2)
     game.start()
 
-def config_armies():
+def config_armies(filename: str) -> None:
     game = Game()
     reader = Reader()
-    armies = reader.read("config.yaml")
+    armies = reader.read(filename)
     for army in armies:
         game.add_army(army)
     game.start()
 
-def main():
+def main() -> None:
     #random_armies()
-    config_armies()
-   
+    config_armies("config.yaml")
 
 
 if __name__ == "__main__":
